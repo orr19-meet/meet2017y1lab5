e1dedf34a53993725e05c26c3f4e6b1ab8e3ef89
orr19-meet <orr19@meet.mit.edu>
2017-07-30 13:52:37 +0300

    Stopping here

---
+++ draw_1d.py
@@ -0,0 +1,40 @@
+c1='!'
+c2='^'
+
+##def fun_1d(n,char):
+##    for fun_1d in range(3):
+##        print(char*n)
+##fun_1d(24,c1)
+
+
+##def draw_2d(n,m,char):
+##    
+##    for i in range(m):
+##        print(char*n)
+##draw_2d(3,7,'x')
+##
+##    
+
+
+##def special_draw_2d(n,m,border,fill):
+##    
+##    
+##    for i in range(1):
+##       print(border*m)
+##    for i in range(n-2):
+##        print(border+fill*(m-2)+border)
+##    for i in range(1):
+##       print(border*m)
+##    return
+##special_draw_2d(7,24,'8','.')
+c=1
+
+def fib(n):
+    for i in range(n):
+        c=c+b
+        print(c)
+
+    return c
+
+    
+    
